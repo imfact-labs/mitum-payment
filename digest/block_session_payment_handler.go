1ff7fc6e2fbbaf754f8c651795f025e04aaaed96
soonkuk <soonkuk@gmail.com>
2026-01-29 15:52:54 +0900

    refact: use currency model BlockSession

--- digest/block_session_payment_handler.go
+++ digest/block_session_payment_handler.go
@@ -1,46 +1,35 @@
 package digest
 
 import (
+	currencydigest "github.com/ProtoconNet/mitum-currency/v3/digest"
 	"github.com/ProtoconNet/mitum-payment/state"
 	"github.com/ProtoconNet/mitum2/base"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (bs *BlockSession) preparePayment() error {
-	if len(bs.sts) < 1 {
-		return nil
-	}
-
-	var paymentDesignModels []mongo.WriteModel
-	var paymentAccountModels []mongo.WriteModel
-	for i := range bs.sts {
-		st := bs.sts[i]
-		switch {
-		case state.IsDesignStateKey(st.Key()):
-			j, err := bs.handlePaymentDesignState(st)
-			if err != nil {
-				return err
-			}
-			paymentDesignModels = append(paymentDesignModels, j...)
-		case state.IsDepositRecordStateKey(st.Key()):
-			j, err := bs.handlePaymentAccountRecordState(st)
-			if err != nil {
-				return err
-			}
-			paymentAccountModels = append(paymentAccountModels, j...)
-		default:
-			continue
+func PreparePayment(bs *currencydigest.BlockSession, st base.State) (string, []mongo.WriteModel, error) {
+	switch {
+	case state.IsDesignStateKey(st.Key()):
+		j, err := handlePaymentDesignState(bs, st)
+		if err != nil {
+			return "", nil, err
 		}
+
+		return DefaultColNamePayment, j, nil
+	case state.IsDepositRecordStateKey(st.Key()):
+		j, err := handlePaymentAccountRecordState(bs, st)
+		if err != nil {
+			return "", nil, err
+		}
+
+		return DefaultColNamePaymentAccount, j, nil
 	}
 
-	bs.paymentDesignModels = paymentDesignModels
-	bs.paymentAccountModels = paymentAccountModels
-
-	return nil
+	return "", nil, nil
 }
 
-func (bs *BlockSession) handlePaymentDesignState(st base.State) ([]mongo.WriteModel, error) {
-	if serviceDesignDoc, err := NewDesignDoc(st, bs.st.Encoder()); err != nil {
+func handlePaymentDesignState(bs *currencydigest.BlockSession, st base.State) ([]mongo.WriteModel, error) {
+	if serviceDesignDoc, err := NewDesignDoc(st, bs.Database().Encoder()); err != nil {
 		return nil, err
 	} else {
 		return []mongo.WriteModel{
@@ -49,8 +38,8 @@ func (bs *BlockSession) handlePaymentDesignState(st base.State) ([]mongo.WriteMo
 	}
 }
 
-func (bs *BlockSession) handlePaymentAccountRecordState(st base.State) ([]mongo.WriteModel, error) {
-	if AccountRecordDoc, err := NewDepositRecordDoc(st, bs.st.Encoder()); err != nil {
+func handlePaymentAccountRecordState(bs *currencydigest.BlockSession, st base.State) ([]mongo.WriteModel, error) {
+	if AccountRecordDoc, err := NewDepositRecordDoc(st, bs.Database().Encoder()); err != nil {
 		return nil, err
 	} else {
 		return []mongo.WriteModel{
